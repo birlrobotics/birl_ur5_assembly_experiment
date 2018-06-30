84da1e7749a896f95c71f45d3c012789d8d1c3e6
sklaw <sk.law.lsq@gmail.com>
2018-06-30 19:55:30 +0800

    Saving

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+setup_args = generate_distutils_setup(
+    packages=[
+        'birl_ur5_assembly_experiment',
+    ],
+    package_dir={'': 'src'}
+)
+
+setup(**setup_args)
