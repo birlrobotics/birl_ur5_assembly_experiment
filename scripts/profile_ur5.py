a7a104fea64f90ee6734045296fb33fdc65c4fd1
sklaw <sk.law.lsq@gmail.com>
2018-06-30 21:22:36 +0800

    Implementing profile_ur5.py

---
+++ scripts/profile_ur5.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import sys
+import rospy
+import moveit_commander as mc
+
+if __name__ == '__main__':
+    mc.roscpp_initialize(sys.argv) 
+    rospy.init_node("profile_ur5_node", anonymous=True)
+
+    robot = mc.RobotCommander()
+    for attr in dir(robot):
+        if attr.startswith("get_"):
+            rospy.loginfo("*"*20)
+            rospy.loginfo(attr)
+            try:
+                rospy.loginfo(getattr(robot, attr)())
+            except:
+                rospy.logerr("Failed to call")
+    
+    
