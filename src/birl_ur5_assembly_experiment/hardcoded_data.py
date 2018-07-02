dd92eaf2c11b3d3c4cc230f9c314a19505bfdc0c
sklaw <sk.law.lsq@gmail.com>
2018-07-02 18:00:45 +0800

    Add one more wall

--- src/birl_ur5_assembly_experiment/hardcoded_data.py
+++ src/birl_ur5_assembly_experiment/hardcoded_data.py
@@ -1,16 +1,18 @@
 from geometry_msgs.msg import Pose, Point, Quaternion
 from math import pi
 
-home_pose = Pose(position=Point(x= 0.347374056271,y= 0.0670839656857,z= 0.575880001466), orientation=Quaternion(x= -0.662637229064,y= 0.381435720055,z= 0.221088127146,w= 0.605424424813))
+home_joint_angles = {'elbow_joint': 0, 'shoulder_pan_joint': -0.20656008768723044, 'wrist_3_joint': -pi/2, 'wrist_1_joint': -pi/2, 'shoulder_lift_joint': -pi/2, 'wrist_2_joint': -pi/2}
 
-ram_fixed_pick_pose = Pose(position=Point(x= 0.619046842606,y= -0.0205555205994,z= 0.240680352798), orientation=Quaternion(x= -0.34275951009,y= 0.642476859947,z= 0.538301547718,w= 0.424229709473))
+get_vision_joint_angles = {'elbow_joint': 2.5755257093621164, 'shoulder_pan_joint': -0.17601812753047863, 'wrist_3_joint': -1.5659576718081425, 'wrist_1_joint': -2.5955913668696757, 'shoulder_lift_joint': -2.4471607016061503, 'wrist_2_joint': -1.5719682040147713}
 
-ram_fixed_place_pose = Pose(position=Point(x= 0.483291959469,y= 0.446571937473,z= 0.222983235428), orientation=Quaternion(x= -0.588390371948,y= 0.253616641848,z= 0.72821750823,w= 0.243258360357))
+ram_fixed_pick_pose = Pose(position=Point(x= 0.451817680005,y= -0.109643459126,z= 0.0228776902664), orientation=Quaternion(x= -0.379426608875,y= 0.598536950665,z= 0.354126316265,w= 0.610232348614))
+
+ram_fixed_place_pose = Pose(position=Point(x= 0.0619293125068,y= 0.398280870516,z= 0.043341303728), orientation=Quaternion(x= -0.693673760483,y= 0.145193951023,z= 0.687426384229,w= 0.158683322592))
 
 joint_constraints_in_degrees = {
     'shoulder_pan_joint': [-22, 68],
-    'shoulder_lift_joint': [-135, -45],
-    'elbow_joint': [-45, 45],
+    'shoulder_lift_joint': [-180, 0],
+    'elbow_joint': [0, 180],
     'wrist_1_joint': [-180, 0],
     'wrist_2_joint': [-135, -45],
     'wrist_3_joint': [-135, -45],
@@ -26,6 +28,12 @@ left_wall_poses = [
     Pose(position=Point(x= -0.0988705763476,y= 0.441884948257,z= 0.603585248101), orientation=Quaternion(x= -0.693678772302,y= -0.292968972292,z= 0.576449733663,w= 0.317308440939)),
     Pose(position=Point(x= -0.163826175967,y= 0.391609718364,z= 0.189631264426), orientation=Quaternion(x= 0.760179978253,y= 0.0883535819475,z= -0.64132964895,w= 0.0549210942937)),
     Pose(position=Point(x= -0.388096786355,y= -0.047544683807,z= 0.41309866761), orientation=Quaternion(x= 0.428966635257,y= 0.630761219358,z= -0.592143774984,w= 0.259795419007))
+]
+
+opposite_wall_poses = [
+    Pose(position=Point(x= 0.568168299346,y= 0.0872185385521,z= 0.198386013786), orientation=Quaternion(x= -0.581156107492,y= 0.404107407163,z= 0.376752910086,w= 0.597504834242)),
+    Pose(position=Point(x= 0.393995670991,y= 0.27672218165,z= 0.234357276234), orientation=Quaternion(x= -0.684419435769,y= 0.177711164359,z= 0.417256640218,w= 0.570863971712)),
+    Pose(position=Point(x= 0.26104379437,y= 0.380654784911,z= 0.112153163096), orientation=Quaternion(x= -0.704658072095,y= 0.0590960947465,z= 0.511240137914,w= 0.488465120969)),
 ]
 
 reverting_statistics = {
