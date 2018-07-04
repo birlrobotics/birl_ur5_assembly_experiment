a6d0cc1c2d48d71cacb55f31154ce9bde58595b2
sklaw <sk.law.lsq@gmail.com>
2018-07-04 18:37:11 +0800

    Saving

---
+++ scripts/experiment_runner.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from smach_based_introspection_framework.online_part import (
+    smach_runner
+)
+from birl_ur5_assembly_experiment.smach_FSM import (
+    assembly_user_defined_sm
+)
+
+from birl_ur5_assembly_experiment.hardcoded_data import (
+    reverting_statistics
+)
+
+if __name__ == '__main__':
+    sm = assembly_user_defined_sm()
+    smach_runner.run(sm, reverting_statistics)
